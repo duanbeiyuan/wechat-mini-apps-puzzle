12bdcbed300042af87a7a010caeb8acda5c1dea9
Dsir666 <95856606+Dsir666@users.noreply.github.com>
2022-05-26 00:55:27 +0800

    Add files via upload

---
+++ pages/demo/demo.js
@@ -0,0 +1,79 @@
+// pages/dbyindex/dbyindex.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        levels:[
+            'pic01.jpg',
+            'pic02.jpg',
+            'pic03.jpg',
+            'pic04.jpg',
+            'pic05.jpg',
+            'pic06.jpg',
+        ]
+    },
+    chooseLevel:function(e){
+        let level=e.currentTarget.dataset.level
+        console.log(level)
+        wx.navigateTo({
+            url:'../game/game?level='+level,
+        })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
